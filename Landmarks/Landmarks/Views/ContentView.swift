2f9070cb5af4da962fd19e62410a33275874425e
Gabriel Rezende <gabrielcastrorezende@gmail.com>
2023-07-27 00:15:15 -0300

    subindo projeto landmarks

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Gabriel Rezende on 26/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
