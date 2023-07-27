88a632ef3fdeab1d0791cd4a443e465aa7b7d3af
Gabriel Rezende <gabrielcastrorezende@gmail.com>
2023-07-27 00:14:15 -0300

    Landmarks SwiftUI project

---
+++ Landmarks/Landmarks/Views/LandMarkRow.swift
@@ -0,0 +1,33 @@
+//
+//  LandMarkRow.swift
+//  Landmarks
+//
+//  Created by Gabriel Rezende on 26/07/23.
+//
+
+import SwiftUI
+
+struct LandMarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandMarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group{
+            LandMarkRow(landmark: landmarks[0])
+            LandMarkRow(landmark: landmarks[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
